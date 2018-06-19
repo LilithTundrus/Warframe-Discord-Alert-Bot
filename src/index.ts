1169ddf15897f069e90843f030559ef51ef0b3bd
Lilith Tundrus <echofox123@gmail.com>
2018-06-18 21:41:17 -0400

    added basic functionality

--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,40 @@ client.on('guildDelete', guild => {
     client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
+// This event will run on every single message received, from any channel or DM.
+client.on('message', async message => {
+    // It's good practice to ignore other bots. This also makes your bot ignore itself
+    if (message.author.bot) return;
+    // Also good practice to ignore any message that does not start with the bot'ss prefix, 
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    // Here we separate our 'command' name, and our 'arguments' for the command. 
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        case 'help':
+        // return helpCommandHandler(message, args);
+        case 'ping':
+            // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+            // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+            const m: any = await message.channel.send('Ping?');
+            m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+            break;
+        default:
+        // If command character + unknown command is given we at least need to let the user know
+        // let errorEmbed = createRichError(`Uknown command: **${command}**`);
+        // return message.channel.send(errorEmbed);
+    }
+});
+
+client.on('error', async error => {
+    logger.error(error);
+    client.user.sendMessage(JSON.stringify(error.message, null, 2), {
+        reply: adminID
+    });
+});
+
+
+// Log the bot in
+client.login(botToken);
