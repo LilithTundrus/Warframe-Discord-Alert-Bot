b406baae704bc7d605ff5a347d5224752179b018
Lilith Tundrus <echofox123@gmail.com>
2018-06-21 22:15:48 -0400

    Working on properly parsing the JSON

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -14,12 +14,24 @@ export function initScheduler(client: Discord.Client, logger: Logger) {
 export function checkForAlertUpdates(client: Discord.Client, logger: Logger) {
     request.get(warframeWorldsstateURL)
         .then((results: string) => {
+            let previousWFData = fs.readFileSync('wfData.json').toString();
+            let previousWFJSON = JSON.parse(previousWFData)
+            
             let discordChannel: any = client.channels.get(alertChannel);
-            let warframeJSON = JSON.parse(results);
-            // console.log(warframeJSON);
-            // JSON.stringify(warframeJSON, null, 2)
-            fs.writeFileSync('wfData.json', results);
-            discordChannel.send(`Wrote the file as 'wfData.json`);
+            let currentWFJSON = JSON.parse(results);
+            // Write something so we always have something to read moving forward
+            // fs.writeFileSync('wfData.json', results);
+
+
+            // Compare the alerts
+            let previousAlerts = previousWFJSON.Alerts;
+            let currentAllerts = currentWFJSON.Alerts;
+
+            console.log(previousAlerts)
+            console.log('\n\n\n\n\n')
+            console.log(currentAllerts)
+
+            discordChannel.send(`Refreshed the JSON manifest`);
         })
         .catch((err) => {
             logger.error(err);
@@ -30,5 +42,5 @@ export function checkForAlertUpdates(client: Discord.Client, logger: Logger) {
             // Message the admin that something went wrong
             let discordChannel: any = client.channels.get(alertChannel);
             discordChannel.send(`${admin}, something went wrong: ${err}`);
-        })
+        });
 }
