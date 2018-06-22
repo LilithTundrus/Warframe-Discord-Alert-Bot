13c5fda8ff0196469668c35e49a87cde9834cce2
Lilith Tundrus <echofox123@gmail.com>
2018-06-22 19:11:24 -0400

    reworking structure

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -3,8 +3,9 @@ import * as request from 'request-promise';
 import * as fs from 'fs'
 import Logger from 'colorful-log-levels';
 import { alertChannel, warframeWorldsstateURL, guildID, adminID } from './config';
+import { warframeAlert } from './interfaces'
 
-// This is the function that will handle getting the alerts on an interval
+// This is the function that will handle getting the alerts on a set interval
 export function initScheduler(client: Discord.Client, logger: Logger) {
     setInterval(() => {
         checkForAlertUpdates(client, logger);
@@ -14,31 +15,32 @@ export function initScheduler(client: Discord.Client, logger: Logger) {
 export function checkForAlertUpdates(client: Discord.Client, logger: Logger) {
     request.get(warframeWorldsstateURL)
         .then((results: string) => {
+            // Read the old alert dataSet
             let previousWFData = fs.readFileSync('wfData.json').toString();
-            let previousWFJSON = JSON.parse(previousWFData)
-
-            let discordChannel: any = client.channels.get(alertChannel);
+            let previousWFJSON = JSON.parse(previousWFData);
+            // Parse the string returned from the request.get
             let currentWFJSON = JSON.parse(results);
-            // Write something so we always have something to read moving forward
 
+            // Shorthand for the worldstate data Alerts section
+            let previousAlerts: warframeAlert[] = previousWFJSON.Alerts;
+            let currentAlerts: warframeAlert[] = currentWFJSON.Alerts;
 
-            // TODO: handle the reads/write properly
-            // Compare the alerts
-            let previousAlerts = previousWFJSON.Alerts;
-            let currentAllerts = currentWFJSON.Alerts;
+            for (let alerts of currentAlerts) {
+                console.log(alerts.Activation.$date.$numberLong)
 
-            console.log(previousAlerts)
-            console.log('\n\n\n\n\n')
-            console.log(currentAllerts)
+            }
 
-            if (previousAlerts !== currentAllerts) {
+            // TODO: find a better comparison method
+            // Comparing the lengths since this is the easiest way to tell that data has changed
+            if (previousAlerts.length !== currentAlerts.length) {
+                logger.debug('Alerts have changed');
+                let discordChannel: any = client.channels.get(alertChannel);
                 discordChannel.send(`Manifest has a new alert entry!`);
             }
 
             // This gets written after a message is sent so current data -> previous file
-            // For the next check
+            // for the next check
             fs.writeFileSync('wfData.json', results);
-
         })
         .catch((err) => {
             logger.error(err);
