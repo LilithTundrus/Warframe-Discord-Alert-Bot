9b788ac57021e9f3c243436575bd57debc9cf6fd
Lilith Tundrus <echofox123@gmail.com>
2018-06-22 19:09:19 -0400

    Working on defining the interfaces for warframe data

---
+++ src/interfaces.ts
@@ -0,0 +1,37 @@
+export interface warframeAlert {
+    _id: { '$oid': string },
+    Activation: { '$date': alertTimeObject },
+    Expiry: { '$date': alertTimeObject },
+    MissionInfo:
+    {
+        missionType: string,
+        faction: string,
+        location: string,
+        levelOverride: string,
+        enemySpec: string,
+        minEnemyLevel: number,
+        maxEnemyLevel: number,
+        difficulty: number,
+        seed: number,
+        maxWaveNum: number,
+        missionReward: missionReward
+    }
+}
+
+
+export interface missionReward {
+    credits: number,
+    items?: string[],
+    countedItems?: itemReward[]
+}
+
+
+export interface itemReward {
+    ItemType: string,
+    ItemCount: number
+}
+
+export interface alertTimeObject {
+    '$numberLong': string
+}
+
